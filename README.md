4bbf41bca5b4e316f39ca1e0a9fcdfbcd4e0cefc
Matt <mhoward13@spu.edu>
2018-03-28 12:05:51 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Frontend-Nanodegree-Neighborhood Map
+====================================
+
+# Regal Cinemas of Washington State
+
+###### Project Overview
+
+Neighborhood Map Project of Udacity's [Front-End Web Developer Nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001). The task was to develop a neighborhood map using the Google Maps API, Knockout JS Library, and Ajax methodology to access a third party API (in my project, I used Foursquare). My neighborhood is Washington State, and here I mapped every Regal Cinemas in WA State
+
+## Getting Started
+
+Download the Zip file, and Open the `index.html` in your browser!
+
+## How To Use
+
+Click on each marker to open up the infowindow. This window has info regarding each theater, and a link to buy tickets. Use the search bar on the left side to narrow down your list of options
+
+## Coming Soon
+
+More implementation on buying tickets, showtimes, and usability coming soon!
---
+++ js/theaters.js
@@ -0,0 +1,222 @@
+/*jshint esversion: 6*/
+
+let theaters = [
+    {
+        title: "Cinebarre Issaquah 8",
+        position: {
+            lat: 47.5471,
+            lng: -122.0524,
+        },
+        streetAddress: "Street Address",
+        city: "City, State Zip",
+        id: '526c7407498e53a9235801b5'
+    },
+    {
+        title: "Cinebarre Mountlake 8",
+        position: {
+            lat: 47.7788,
+            lng: -122.3150,
+        },
+        id: '4a57b93bf964a520abb61fe3'
+    },
+    {
+        title: "Regal Cinemas Alderwood 7 & RPX",
+        position: {
+            lat: 47.8330,
+            lng: -122.2805,
+        },
+        id: '49e960e6f964a520f6651fe3'
+    },
+    {
+        title: "Regal Cinemas Auburn 17",
+        position: {
+            lat: 47.2994,
+            lng: -122.2407,
+        },
+        id: '4af61065f964a520ba0022e3'
+    },
+    {
+        title: "Regal Cinemas Barkley Village 16 IMAX & RPX",
+        position: {
+            lat: 48.7703,
+            lng: -122.4495,
+        },
+        id: '505b29037ab4b1ee19260f87'
+    },
+    {
+        title: "Regal Cinemas Bella Bottega 11",
+        position: {
+            lat: 47.6807,
+            lng: -122.1229,
+        },
+        id: '4ac80b62f964a5205abb20e3'
+    },
+    {
+        title: "Regal Cinemas Cascade 16 IMAX & RPX",
+        position: {
+            lat: 45.6126,
+            lng: -122.5067,
+        },
+        id: '4b22e6faf964a520f64f24e3'
+    },
+    {
+        title: "Regal Cinemas 99 11",
+        position: {
+            lat: 45.6883,
+            lng: -122.6627,
+        },
+        id: '4a74f76df964a52027e01fe3'
+    },
+    {
+        title: "Regal Cinemas Columbia Mall 8",
+        position: {
+            lat: 46.2249,
+            lng: -119.2274,
+        },
+        id: '4b786ac6f964a520b8cc2ee3'
+    },
+    {
+        title: "Regal Cinemas Everett Mall 16 & RPX",
+        position: {
+            lat: 47.9094,
+            lng: -122.2138,
+        },
+        id: '4a96d290f964a5201e2720e3'
+    },
+    {
+        title: "Regal Cinemas Issaquah Highlands 12 IMAX & RPX",
+        position: {
+            lat: 47.5439,
+            lng: -122.0180,
+        },
+        id: '51534044e4b0461e2cece255'
+    },
+    {
+        title: "Regal Cinemas Lakewood 15 & RPX",
+        position: {
+            lat: 47.1801,
+            lng: -122.4686,
+        },
+        id: '4a02703af964a5204e711fe3'
+    },
+    {
+        title: "Regal Cinemas Longston Place 14",
+        position: {
+            lat: 47.1354,
+            lng: -122.2911,
+        },
+        id: '4b48ee2cf964a520e15d26e3'
+    },
+    {
+        title: "Regal Cinemas Martin Village 16 & IMAX",
+        position: {
+            lat: 47.0508,
+            lng: -122.8135,
+        },
+        id: '4b36ed99f964a5204d3e25e3'
+    },
+    {
+        title: "Regal Cinemas Marysville 14",
+        position: {
+            lat: 48.0837,
+            lng: -122.1685,
+        },
+        id: '4ad00f86f964a52078d720e3'
+    },
+    {
+        title: "Regal Cinemas Meridian 16",
+        position: {
+            lat: 47.6118,
+            lng: -122.3337,
+        },
+        id: '41be2d00f964a520851e1fe3'
+    },
+    {
+        title: "Regal Cinemas Northtown Mall 12",
+        position: {
+            lat: 47.7020,
+            lng: -117.4102,
+        },
+        id: '4b7f60bbf964a520182b30e3'
+    },
+    {
+        title: "Regal Cinemas Parkway Plaza 12",
+        position: {
+            lat: 32.8054,
+            lng: -116.9676,
+        },
+        id: '4a6ceb33f964a52011d21fe3'
+    },
+    {
+        title: "Regal Cinemas Poulsbo 10",
+        position: {
+            lat: 47.7440,
+            lng: -122.6592,
+        },
+        id: '4a86196af964a5206e0020e3'
+    },
+    {
+        title: "Regal Cinemas South Hill 6",
+        position: {
+            lat: 47.1589,
+            lng: -122.2974,
+        },
+        id: '4d166aab81cea35dcd3fe0ec'
+    },
+    {
+        title: "Regal Cinemas South Sound Cinema 10",
+        position: {
+            lat: 47.5351,
+            lng: -122.6058,
+        },
+        id: '4a860516f964a5201c0020e3'
+    },
+    {
+        title: "Regal Cinemas Spokane Valley 12",
+        position: {
+            lat: 47.6735,
+            lng: -117.2093,
+        },
+        id: '4b5e7afff964a520519029e3'
+    },
+    {
+        title: "Regal Cinemas Tall Firs 10",
+        position: {
+            lat: 47.1660,
+            lng: -122.1537,
+        },
+        id: '4bc8c0f4ab62d13a63f317d4'
+    },
+    {
+        title: "Regal Cinemas The Landing 14 & RPX",
+        position: {
+            lat: 47.4989,
+            lng: -122.2035,
+        },
+        id: '49fb6cedf964a520466e1fe3'
+    },
+    {
+        title: "Regal Cinemas Thornton Place 14 & IMAX",
+        position: {
+            lat: 47.7028,
+            lng: -122.3253,
+        },
+        id: '4a52438ef964a52075b11fe3'
+    },
+    {
+        title: "Regal Cinemas Three Rivers Mall 12 & RPX",
+        position: {
+            lat: 46.1388,
+            lng: -122.9002,
+        },
+        id: '4d2dee18774f76eb1cfd9533'
+    },
+    {
+        title: "Regal Cinemas Vancouver Plaza",
+        position: {
+            lat: 45.6477,
+            lng: -122.5953,
+        },
+        id: '4ba813a1f964a520feca39e3'
+    }
+];
